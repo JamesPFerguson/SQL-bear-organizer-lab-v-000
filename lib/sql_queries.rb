cc66db72dadce3dc94e3bef9c362b195fa01b2c6
James Ferguson <ncjamesferguson@gmail.com>
2018-01-10 21:05:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT bear.name FROM bear WHERE bears.name = null;"
+  "SELECT bear.name FROM bears WHERE bears.name = null;"
 end
